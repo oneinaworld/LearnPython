1aa9eafc5749942d83badf85e7479e3e49efe08c
oneinaworld <kjgfdlo@163.com>
2016-12-23 14:23:32 +0800

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -23,8 +23,53 @@ print "what is 5-7?",5-7
 
 print "Oh,that's why it's False."
 
+#尝试同时打印文本与运算结果
 print "How about some more."
 
 print "Is it greater?",5>-2
 print "Is it greater or equal?",5>=-2
 print "Is it less or equal?",5<=-2
+
+
+#try to do math
+print 3=5
+print 3+9583-1917*4
+
+#to use a float point number
+print 3.3+4.2
+print 3.62*932-32.0
+
+
+
+#rewrite the lines in floating number
+# The start of the program.
+print "I will count my chickens:"
+
+#Count the number of hens and roosters.
+print "Hens",25.0+30.0/6.0
+print "Roosters",100.0-25.0*3.0%4.0
+
+#Next to count the eggs.
+print "Now I will count the eggs:"
+
+#The number of the eggs.
+print 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0
+
+#Next to compare two equation.
+print "Is it true that 3+2<5-7?"
+
+#Compare 3+2 and 5-7.The result.
+print 3.0+2.0<5.0-7.0
+
+#The result of each quation and Why the result is >.
+print "What is 3+2?",3.0+2.0
+print "what is 5-7?",5.0-7.0
+
+print "Oh,that's why it's False."
+
+#尝试同时打印文本与运算结果
+print "How about some more."
+
+print "Is it greater?",5.0>-2.0
+print "Is it greater or equal?",5.0>=-2.0
+print "Is it less or equal?",5.0<=-2.0
