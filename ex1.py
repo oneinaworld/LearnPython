d1ed121c3f11a0b77554fa0f352484f5c2393d18
oneinaworld <kjgfdlo@163.com>
2016-12-23 11:16:38 +0800

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print "Hello World!"
+print "hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay!Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print "A new comer I am."
+# Oh!Screen is full of unknown items!
+# What does "Make your script print only one of the lines." mean?
