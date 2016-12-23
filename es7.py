b185dc04962a6a362cf37fee52f05f1c12d9807f
oneinaworld <kjgfdlo@163.com>
2016-12-23 19:30:51 +0800

    Create es7.py

---
+++ es7.py
@@ -0,0 +1,47 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "."*10  #that's ............
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+# above is a practice for strings
+
+
+#Go back, write a comment on what each line does
+print "Mary had a little lamb.",
+print "Its fleece was white as %s." % 'snow',
+print "And everywhere that Mary went.",
+print "."*10  #that's ............
+
+end1 = "C",
+end2 = "h",
+end3 = "e",
+end4 = "e",
+end5 = "s",
+end6 = "e",
+end7 = "B",
+end8 = "u",
+end9 = "r",
+end10 = "g",
+end11 = "e",
+end12 = "r",
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+# a comment in the end of lines will get the lines into one line
+# comments in the end of characters will seperate these characters
+
+
