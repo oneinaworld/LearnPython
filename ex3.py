cd22086f81bf13329e1b141609054f11765f2b6c
oneinaworld <kjgfdlo@163.com>
2016-12-23 12:02:52 +0800

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,16 +1,23 @@
+# The start of the program.
 print "I will count my chickens:"
 
+#Count the number of hens and roosters.
 print "Hens",25+30/6
 print "Roosters",100-25*3%4
 
+#Next to count the eggs.
 print "Now I will count the eggs:"
 
+#The number of the eggs.
 print 3+2+1-5+4%2-1/4+6
 
+#Next to compare two equation.
 print "Is it true that 3+2<5-7?"
 
+#Compare 3+2 and 5-7.The result.
 print 3+2<5-7
 
+#The result of each quation and Why the result is >.
 print "What is 3+2?",3+2
 print "what is 5-7?",5-7
 
