9696a849f2b002b1f9760cf488dc6f67a00b28dd
oneinaworld <kjgfdlo@163.com>
2016-12-26 10:48:14 +0800

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# Prompting people is 提示。
+# Rewrite ex11 using raw_input to do all the prompting.
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "So, you're %r old, %r tall and %r heavy."%(age,height,weight)
+
