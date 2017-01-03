13ec1308f7682bca308b1c1f684ee103b348b393
oneinaworld <kjgfdlo@163.com>
2017-01-03 21:59:09 +0800

    Create ex0.py

---
+++ ex0.py
@@ -0,0 +1,5 @@
+# I found the ideas are good so I wrote it here.
+# If someone tells you to stop at a specific exercise in this book or to skip certain ones, you should ignore that person.
+# Anyone trying to hide knowledge from you, or worse, make you get it from them instead of through you own efforts, is trying to make you depend on them for your skills.
+			
+				
