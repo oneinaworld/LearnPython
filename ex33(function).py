882088c5afce470766990353fa2efb808b735586
oneinaworld <kjgfdlo@163.com>
2017-01-10 21:37:24 +0800

    Update ex33(function).py

--- ex33(function).py
+++ ex33(function).py
@@ -1,4 +1,4 @@
-def numprint(a):
+def numprint1(a):
     i = 0
     numbers = []
 
@@ -10,6 +10,24 @@ def numprint(a):
        print "Numbers now:", numbers
        print "At the bottom i is %d" % i
     
+    print "The numbers:"
+
+    for num in numbers:
+       print num
+        
+    
+def numprint2(a, b):
+    i = 0
+    numbers = []
+
+    while i < a:
+       print "At the top i is %d" % i
+       numbers.append(i)
+
+       i = i + b
+       print "Numbers now:", numbers
+       print "At the bottom i is %d" % i
+
     print "The numbers:"
 
     for num in numbers:
