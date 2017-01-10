f2597990e7e27b8163955906154653f516615265
oneinaworld <kjgfdlo@163.com>
2017-01-10 17:43:09 +0800

    Update ex31.py

--- ex31.py
+++ ex31.py
@@ -1,9 +1,9 @@
-print "you enter a dark room with two doors. Do you go through door #1 or dor #2?"
+print "You enter a dark room with two doors. Do you go through door #1 or door #2?"
 
 door  = raw_input(">")
 
 if door == "1":
-    print "There's a giant bear eatinga cheese cake. What do you do?"
+    print "There's a giant bear here eating a cheese cake. What do you do?"
     print "1. Take the cake."
     print "2. Scream at the bear."
     
@@ -27,7 +27,7 @@ elif door =="2":
 	if insanity == "1" or insanity == "2":
 		print "Your body survives powered by a mind of jello. Good job!"
 	else:
-		print "The insinity rots your eyes into a pool of muck. Good job!"
+		print "The insanity rots your eyes into a pool of muck. Good job!"
 		
 else:
 	print "You stumble around and fall on a knife and die. Good job!"
