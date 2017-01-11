c56d21e7141109fab9a941882a6a3a82f43db741
oneinaworld <kjgfdlo@163.com>
2017-01-11 15:54:04 +0800

    Update ex35.py

--- ex35.py
+++ ex35.py
@@ -14,7 +14,8 @@ def gold_room():
         exit(0)
     else:
         dead("You greedy bastard!")
-        
+
+		
 def bear_room():
     print "There is a bear here."
     print "The bear has a bunch of honey."
@@ -22,51 +23,53 @@ def bear_room():
     print "How are you going to move the bear?"
     bear_moved = False
     
-    while True:
+    while True:  # what does this mean? What true?
         choice = raw_input(">")
         
         if choice == "take honey":
-            dead("The bear looks at you then slaps you face off.")
+            dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
-						print "The bear has moved from the door. You can go throuth it now."
-						bear_moved = True
-				elif choice == "taunt bear" and bear_moved:
-						dead("The bear gets pissed off and chews your leg off.")
-				elif choice == "open door" and bear_moved:
-						gold_room()
-				else:
-						print "I got no idea what that means."
+			print "The bear has moved from the door. You can go throuth it now."
+			bear_moved = True
+		elif choice == "taunt bear" and bear_moved:
+			dead("The bear gets pissed off and chews your leg off.")
+		elif choice == "open door" and bear_moved:
+			gold_room()
+		else:
+			print "I got no idea what that means."
 						
-	def cthulhu_room():
-			print "Here you see the great evil Cthulhu."
-			print "He, it, whatever stares at you and you go insane."
-			print "Do you flee for your life or eat your head?"
+def cthulhu_room():
+	print "Here you see the great evil Cthulhu."
+	print "He, it, whatever stares at you and you go insane."
+	print "Do you flee for your life or eat your head?"
 			
-			choice = raw_input(">")
+	choice = raw_input(">")
 			
-			if "flee" in choice:
-					start()
-			elif "head" in choice:
-					dead("Well that was tasty!")
-			else:
-					cthulhu_room()
+	if "flee" in choice:
+		start()
+	elif "head" in choice:
+		dead("Well that was tasty!")
+	else:
+		cthulhu_room()
 			
 def dead(why):
-		print why, "Good job!"
-		exit(0)
+	print why, "Good job!"
+	exit(0)
 		
 def start():
-		print "You are in a dark room."
-		print "There is a door to your right and left."
-		print "Which one do you take?"
+	print "You are in a dark room."
+	print "There is a door to your right and left."
+	print "Which one do you take?"
 		
-		choice = raw_input(">")
+	choice = raw_input(">")
 		
-		if choice == "left":
-				bear_room()
-		elif choice == "right":
-				cthulhu_room()
-		else:
-				dead("You stumble around the room until you starve.")
+	if choice == "left":
+		bear_room()
+	elif choice == "right":
+		cthulhu_room()
+	else:
+		dead("You stumble around the room until you starve.")
+
+				
 				
 start()
