ad646f1b70ea224c963b26901fa8d2e7a7100583
oneinaworld <kjgfdlo@163.com>
2017-01-20 15:48:33 +0800

    Update oop_test.py

--- oop_test.py
+++ oop_test.py
@@ -28,16 +28,19 @@ if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 else:
     PHRASE_FIRST = False
-  
+
+# 第一步
 # load up the words from the website
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(word.strip())    # strip函数，移除word头尾指定字符，此处移除空格。
+    # 这里其实就是用word列表里的单词去填充WORDS。
     
-
-def convert(snippet, phrase):
-    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]  #   
+def convert(snippet, phrase):   # 此函数的用途为，
+    # 1：
+    class_names = [w.capitalize() for w in 
+                   random.sample(WORDS, snippet.count("%%%"))]  # snippet中有几个%%%，就从WORDS中拿出几个元素，组成列表
     """random模块。
-    random.sample  从WORD中随机获得snippet.count个元素且不改变WORD。
+    random.sample  从WORDS中随机获得snippet.count个元素组成新列表，且不改变WORD。
     
     capitalize函数。
     返回w的一个副本，只有第一个字母大写。
@@ -45,14 +48,17 @@ def convert(snippet, phrase):
     count函数。
     在snippet中搜索%%%出现的次数。
     """
-    other_names = random.sample(WORDS, snippet.count("***"))    # 
+    other_names = random.sample(WORDS, snippet.count("***"))    # snippet中有几个***，就从WORDS中拿出几个元素，组成列表
     results = []
     param_names = []  
     
-    for i in range(0, snippet.count("@@@")):   # 在snippet的个数范围内
-        param_count = random.randint(1,3)  #random.randint，在1-3范围内生成随机整数。
-        param_names.append(','.join(random.sample(WORDS, param_count)))  #扩充param_names列表。用从WORDS列表中的获取元素并且用“，”连接的。
+    # 2：
+    for i in range(0, snippet.count("@@@")):   # 在snippet中有几个@@@，拿出一个
+        param_count = random.randint(1,3)  # random.randint，在1-3范围内生成随机整数。参数个数为1-3随机。
+        param_names.append(','.join(random.sample(WORDS, param_count)))  
+        # 有几个参数，就从WORDS中拿出几个元素，并且用“，”连接，结果放入列表
         
+    # 3：
     for sentence in snippet, phrase:
         result = sentence[:]
         
@@ -73,15 +79,18 @@ def convert(snippet, phrase):
     return results
         
 
+# 第二步
 # keep going until they hit CTRL-D
 try:
     while True:
-        snippets = PHRASES.keys()  # keys函数，返回字典所有的key。
+        # 1：
+        snippets = PHRASES.keys()  # keys函数，返回字典所有的key。即提取PHRASES中的所有键组成snippets列表。
         random.shuffle(snippets)  # random.shuffle，把snippets列表中的元素打乱更新。
         
-        for snippet in snippets:
-            phrase = PHRASES[snippet]
-            question, answer = convert(snippet, phrase)
+        # 2：
+        for snippet in snippets:    # 从snippets中取出一个元素snippet，即PHRASES中的键。
+            phrase = PHRASES[snippet]    # 从PHRASES中取出对应的值phrase
+            question, answer = convert(snippet, phrase)   # 进入convert函数。
             if PHRASE_FIRST:
                 question, answer = answer, question
             
