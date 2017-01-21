7be4466d96982f3117eeaeff2aaa16a74dd9307a
oneinaworld <kjgfdlo@163.com>
2017-01-21 15:50:19 +0800

    Create ex43.py

---
+++ ex43.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+
+class Scene(object):      # scene类
+    def enter(self):
+        pass
+        
+        
+class Engine(object):
+    def __inif__(self, scene_map):
+        pass
+        
+    def play(self):
+        pass
+        
+       
+class Death(Scene):        # death是scene类
+    def enter(self):
+        pass
+        
+        
+class CentralCorridor(Scene):    # centralcorridor是scene类
+    def enter(self):
+        pass
+        
+        
+class LaserWeaponArmory(Scene):     # laserweaponarmory是scene类
+    def enter(self):
+        pass
+        
+        
+class TheBridge(Scene):        # thebridge是scene类
+    def enter(self):
+        pass
+        
+        
+class EscapePod(Scene):        # escapepod是scene类
+    def enter(self):
+        pass
+        
+        
+class Map(object):
+    def __inif__(self, start_scene):
+        pass
+        
+    def next_scene(self, scene_name):
+        pass
+        
+    def opening_scene(self):
+        pass
+        
+        
+        
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
